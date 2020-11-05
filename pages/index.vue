713e9a6071242029edf35f3bade51906974cd521
Joan <davidjoan53@gmail.com>
2020-11-05 18:05:48 -0500

    Principal sin fondo

--- pages/index.vue
+++ pages/index.vue
@@ -54,7 +54,7 @@ export default {
   margin: 15% 0 0 5%;
   padding: 3% 0 1% 3%;
   width: 36.5%;
-  background-image: url('../static/fondo_madera.jpg');
+  /*background-image: url('../static/fondo_madera.jpg');*/
 }
 
 .informacion_h1{
